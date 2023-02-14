08f8ab2ade1df12f7d17085f16677e82f47e0ec5
Viktor Velychko <Cezn.Vi@gmail.com>
2023-02-14 19:09:12 +0200

    WIP

---
+++ SimpleCalc/AboutForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SimpleCalc
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_Ok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Кулькулятор v1.0.0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(315, 185);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Я описание этой программы";
+            // 
+            // button_Ok
+            // 
+            this.button_Ok.Location = new System.Drawing.Point(135, 245);
+            this.button_Ok.Name = "button_Ok";
+            this.button_Ok.Size = new System.Drawing.Size(75, 23);
+            this.button_Ok.TabIndex = 2;
+            this.button_Ok.Text = "OK";
+            this.button_Ok.UseVisualStyleBackColor = true;
+            this.button_Ok.Click += new System.EventHandler(this.button_Ok_Click);
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 280);
+            this.Controls.Add(this.button_Ok);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.Text = "AboutForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_Ok;
+    }
+}
---
+++ SimpleCalc/MainForm.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SimpleCalc
+{
+    public partial class MainForm : Form
+    {
+        private string[] _buttonNames =
+        {
+            "⬅", "CE", "C", "±", "√", "7", "8", "9", "/", "%", "4", "5", "6", "*", "1/x", "="
+        };
+        private List<Button> _buttonList = new List<Button>();
+        private const int _bitDepth = 8;
+        private string _currOperation = "";
+        private string _leftOperand = "";
+        private string _rigthOperand = "";
+        public MainForm()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+            this.Load += MainForm_Load;
+            InitButtons();
+        }
+
+        private void InitButtons()
+        {
+            int ind = 0;
+            int posX = 12;
+            int posY = 50;
+            foreach (var nameButton in _buttonNames)
+            {
+                Button tmp = new Button();
+                tmp.Text = nameButton;
+                tmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+
+                tmp.Size = new Size(60, 40);
+                tmp.Location = new Point(posX, posY);
+                posX = posX + tmp.Size.Width + 6;
+
+                ind++;
+                if (ind % 5 == 0)
+                {
+                    posY += tmp.Size.Height + 6;
+                    posX = 12;
+                }
+
+                panel.Controls.Add(tmp);
+                _buttonList.Add(tmp);
+                tmp.Click += Btn_Click;
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            //MessageBox.Show(btn.Text);
+
+            switch (btn.Text)
+            {
+                case "0":
+                    {
+
+                        break;
+                    }
+                case ".":
+                    {
+
+                        break;
+                    }
+                case "7":
+                    {
+                        if(_currOperation.Length == 0)      //left
+                        {
+                            if(_leftOperand.Length < _bitDepth)
+                            {
+                                _leftOperand += "7";
+                                textBoxResult.Text = _leftOperand;
+                            }
+                        }
+                        else {                              //right
+                            if (_rigthOperand.Length < _bitDepth)
+                            {
+                                _rigthOperand += "7";
+                                textBoxResult.Text = _rigthOperand;
+                            }
+                        }
+                        break;
+                    }
+                case "*":
+                    {
+                        _currOperation = "*";
+                        textBoxResult.Text = "";
+                        break;
+                    }
+                case "=":
+                    {
+                        double lOper = 0.0;
+                        double rOper = 0.0;
+                        double result = 0.0;
+                        switch (_currOperation)
+                        {
+                            case "*":
+                                {
+                                    lOper = double.Parse(_leftOperand);
+                                    rOper = double.Parse(_rigthOperand);
+                                    result = lOper * rOper;
+                                    textBoxResult.Text = result.ToString();
+                                    break;
+                                }
+                            case "+":
+                                {
+                                    break;
+                                }
+                            case "-":
+                                {
+                                    break;
+                                }
+                            case "√":
+                                {
+
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_MC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_MR_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_MS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutForm = new AboutForm();
+            aboutForm.StartPosition = FormStartPosition.CenterParent;
+            aboutForm.ShowDialog();
+        }
+    }
+}
